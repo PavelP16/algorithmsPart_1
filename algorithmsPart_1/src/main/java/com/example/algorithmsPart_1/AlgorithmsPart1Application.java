31c7269221490c7acba68caee14e5f1e60e19716
Pavel <pavelpulle1@gmail.co>
2024-09-16 22:27:02 +0300

    Initial commit

---
+++ algorithmsPart_1/src/main/java/com/example/algorithmsPart_1/AlgorithmsPart1Application.java
@@ -0,0 +1,13 @@
+package com.example.algorithmsPart_1;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class AlgorithmsPart1Application {
+
+	public static void main(String[] args) {
+		SpringApplication.run(AlgorithmsPart1Application.class, args);
+	}
+
+}
---
+++ algorithmsPart_1/src/test/java/com/example/algorithmsPart_1/AlgorithmsPart1ApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.algorithmsPart_1;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class AlgorithmsPart1ApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
